bbebc30794ce9847fc51bd491a9dc9bca088b195
Vitalie Jurjiu <vjurjiu@EN30325-2.local>
2016-03-16 11:27:04 +0200

    Refactor request method for array response

--- WeatherApp/Utilities/DataParser.swift
+++ WeatherApp/Utilities/DataParser.swift
@@ -38,63 +38,73 @@ extension MyEndpoint : Endpoint {
     public var path : String {
         switch self {
         case .Weather :
-            return "weather"
-            
+            return "weather/"
         case .Search :
-            return "find"
+            return "find/"
         }
     }
-        
+    
     public var method : HTTPMethod {
-            return .GET
+        return .GET
     }
-}
-
-func performRequest(endpoint: MyEndpoint, parameters: [String: AnyObject]? = nil) {
-    let type = endpoint.method
-    let URL = endpoint.baseURL.URLByAppendingPathExtension(endpoint.path)
-    
-    // ["q": city, "appid": Url.apiKey, "units": Units.Celsius.rawValue, "type": "like", "mode":"json"]
-    Alamofire.request(.GET, URL.baseURL.URLByAppendingPathExtension(URL.path), parameters: parameters, encoding: .URLEncodedInURL).responseObject() { (response: Alamofire.Response <Response, NSError>) in
-        
-        print(response.result.value?.forecastsArray)
-        print(response.result.error)
-        
-    }
-
     
 }
 
+final class DataParser {
+    static var parameters : [String : AnyObject]?
+    static let endpoint = MyEndpoint.Search
+
+    //let parameters = ["q": parameters["city"], "appid": parameters["key"], "units": parameters["units"], "type": parameters["accuracy"], "mode": parameters["mode"]]
+    
+    static func performRequest(endpoint: MyEndpoint, parameters: [String: AnyObject]? = nil) {
+        
+        
+        let URL = endpoint.baseURL.URLByAppendingPathExtension(endpoint.path)
+        
+        
+        Alamofire.request(.GET, URL , parameters: parameters, encoding: .URLEncodedInURL).responseArray("list") { (response: Response<[Forecast], NSError>) in
+            //            guard response.result.isSuccess else {
+            //                print("Error while fetching tags: \(response.result.error)")
+            //                return
+            //            }
+            //
+            //            guard let responseJSON = response.result.value as? [String: AnyObject] else {
+            //                print("Invalid tag information received from service")
+            //                return
+            //            }
+        }
+    }
+}
 
 
 
 //    static func requestDataForCity(city: String) {
-//        
+//
 //        let URL = MyEndpoint.Search
-//        
-//        
+//
+//
 //        Alamofire.request(.GET, Url.base + Url.version + Url.weather, parameters: ["q": city, "appid": Url.apiKey, "units": Units.Celsius.rawValue, "type": "like"]).responseArray() { ( response: Response<[Forecast], NSError>) -> Void in
 //            print(response.result.value)
 //            print(response.result.error)
 //
 //        }
 //            responseArray<Forecast>(keyPath: "list", { (response: Response<[Forecast], NSError>) in
-//            
+//
 //        }
 
 //        Alamofire.request(.GET, Url.base + Url.version + Url.weather, parameters: ["q": city, "appid": Url.apiKey, "units": Units.Celsius.rawValue, "type": "like"]). { response -> Void in
-//            
+//
 //            guard response.result.isSuccess else {
 //                print("Error while fetching tags: \(response.result.error)")
 //                return
 //            }
-//            
+//
 //            guard let responseJSON = response.result.value as? [String: AnyObject] else {
 //                print("Invalid tag information received from service")
 //                return
 //            }
-//            
+//
 //            let forecasts : Forecast = Mapper<Forecast>().map(responseJSON)!
-//            
+//
 //            print("The weather in \(forecast.cityName) is \(forecast.weatherDescription) and the temperature is \(forecast.temp)")
 //        }
